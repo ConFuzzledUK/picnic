b2f7460879ff085a1ba93394702e1848a6a99343
Nidonocu <nidonocu@gmail.com>
2015-08-16 02:13:19 +0100

    Adjust typings

---
+++ Picnic 10/typings/tsd.d.ts
@@ -0,0 +1,14 @@
+
+/// <reference path="express/express.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
+/// <reference path="cookie-parser/cookie-parser.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="jqueryui/jqueryui.d.ts" />
+/// <reference path="morgan/morgan.d.ts" />
+/// <reference path="mysql/mysql.d.ts" />
+/// <reference path="serve-favicon/serve-favicon.d.ts" />
+/// <reference path="underscore.string/underscore.string.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
